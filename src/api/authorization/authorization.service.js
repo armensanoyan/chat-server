6b501026e125a92dab0dbbb93c56bedb3c7cd365
armensanoyan <armensanoyan@gmail.com>
2024-06-10 22:12:06 +0400

    refactor: Update authorization API routes and middleware

--- src/api/authorization/authorization.service.js
+++ src/api/authorization/authorization.service.js
@@ -1,34 +1,40 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import { AuthorizationError } from '../../utils/errors.js'
-import { addUser, getUserByEmail } from '../../models/class-methods/users.js'
-import { handleAdd, handleGet } from '../../utils/success-handler.js'
+import { addUser, getUserByEmail, softDeleteUser } from '../../models/class-methods/users.js'
+import { handleAdd, handleGet, handleDelete } from '../../utils/success-handler.js'
 import { JWT_SECRET } from '../../config/config.js'
 
 export const register = async (req, res, next) => {
   const userData = req.body
-  const userFromDb = await getUserByEmail(userData.email)
+  try {
+    const userFromDb = await getUserByEmail(userData.email)
 
-  if (userFromDb) {
-    return next(new AuthorizationError('User already exists'))
-  } else {
-    const user = await addUser({ ...userData, password: bcrypt.hashSync(userData.password, 8) })
-    res.status(201).json({ user })
+    if (userFromDb) {
+      return next(new AuthorizationError('User already exists'))
+    } else {
+      const { password, ...user } = await addUser({ ...userData, password: bcrypt.hashSync(userData.password, 8) })
 
-    return handleAdd(res, user)
+      return handleAdd(res, user)
+    }
+  } catch (error) {
+    return next(error)
   }
 }
 
 export const login = async (req, res, next) => {
   const { email, password } = req.body
-  const user = await getUserByEmail(email)
+  try {
+    const user = await getUserByEmail(email)
 
-  if (user && bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign({ email, userId: user.id }, JWT_SECRET, { expiresIn: '1h' })
-
-    return handleGet(res, { token })
-  } else {
-    return next(new AuthorizationError('Invalid credentials'))
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign({ email, userId: user.id }, JWT_SECRET, { expiresIn: '1h' })
+      return handleGet(res, { token })
+    } else {
+      return next(new AuthorizationError('Invalid credentials'))
+    }
+  } catch (error) {
+    return next(error)
   }
 }
 
@@ -38,7 +44,12 @@ export const logout = (req, res, next) => {
   return handleGet(res, 'Logged out')
 }
 
-export const deleteUser = (req, res, next) => {
-  // Delete user logic here
-  return handleGet(res, 'Deleted')
+export const deleteUser = async (req, res, next) => {
+  try {
+    await softDeleteUser(req.userId)
+
+    return handleDelete(res, 'Deleted')
+  } catch (error) {
+    return next(error)
+  }
 }
--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -3,7 +3,7 @@ import { UnauthorizedError } from '../utils/errors.js'
 import { JWT_SECRET } from '../config/config.js'
 
 export const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization').split(' ')[1]
+  const token = req.header('Authorization')?.split(' ')[1]
   if (!token) return res.status(401).json({ error: 'Access denied' })
   try {
     const decoded = jwt.verify(token, JWT_SECRET)
